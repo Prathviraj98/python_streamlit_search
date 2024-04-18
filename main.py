97ee82e80c82c08592cfaec109512874ce2b1065
Prathviraj <95234838+Prathviraj98@users.noreply.github.com>
2024-04-18 16:24:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,32 @@
 """simple search forntend"""
 import webbrowser
 import streamlit as st
-import json
-from streamlit_lottie import st_lottie
 site=""
 
-def load_lottiefile(filepath: str):
-  with open(filepath, "r",encoding="UTF-8") as f:
-    return json.load(f)
-anime=load_lottiefile("Animation.json")
+
 st.set_page_config(page_title="Python Search",
 page_icon=":mag:",
-layout="centered")
-col1, col2= st.columns(2)
-with col1:
+layout="wide")
+col1, col2,col3= st.columns(3)
+col11, col12,col13= st.columns(3)
+col21, col22,col23= st.columns(3)
+with col2:
     st.title("Python Search")
+with col12:    
+    query = st.text_input(
+    "Enter your query..."
+    )
     website=st.radio(
     "select your search platform",
     ["DuckDuckGo", "Brave", "Google", "Youtube","Wikipedia"],
     index=0,
+    
     )
-    query = st.text_input(
-    "Enter your query..."
-    )
+ 
 
 
 with col2:
-    st_lottie(
-    anime,
-    speed=2,
-    reverse=False,
-    loop=True,
-    quality="high",
-    height=262,
-    key=None,
-    )
+
     
     if query:
         match website:
@@ -47,7 +39,8 @@ with col2:
             case "Youtube": site="https://www.youtube.com/results?search_query="+query
         
             case "Wikipedia": site="https://en.wikipedia.org/wiki/"+query
-    st.link_button("Search", site)
+with col23:
+        st.link_button("Search", site)
 
 
         
