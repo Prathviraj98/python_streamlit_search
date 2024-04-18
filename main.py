3e907064a7d2bf4adde62c265e02acb65b9719ab
Prathviraj <95234838+Prathviraj98@users.noreply.github.com>
2024-04-18 15:48:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import webbrowser
 import streamlit as st
 import json
 from streamlit_lottie import st_lottie
+site=""
 
 def load_lottiefile(filepath: str):
   with open(filepath, "r",encoding="UTF-8") as f:
@@ -11,41 +12,42 @@ anime=load_lottiefile("Animation.json")
 st.set_page_config(page_title="Python Search",
 page_icon=":mag:",
 layout="centered")
-st.title("Python Search")
-st_lottie(
+col1, col2= st.columns(2)
+with col1:
+    st.title("Python Search")
+    website=st.radio(
+    "select your search platform",
+    ["DuckDuckGo", "Brave", "Google", "Youtube","Wikipedia"],
+    index=0,
+    )
+    query = st.text_input(
+    "Enter your query..."
+    )
+
+
+with col2:
+    st_lottie(
     anime,
     speed=2,
     reverse=False,
     loop=True,
     quality="high",
-    height=200,
+    height=262,
     key=None,
     )
-query = st.text_input(
-    "Enter your query..."
-    )
+    
+    if query:
+        match website:
+            case "Google": site="https://www.google.com/search?q="+query
+        
+            case "Brave": site="https://search.brave.com/search?q="+query
+        
+            case "DuckDuckGo": site="https://duckduckgo.com/?q="+query
+        
+            case "Youtube": site="https://www.youtube.com/results?search_query="+query
+        
+            case "Wikipedia": site="https://en.wikipedia.org/wiki/"+query
+    st.link_button("Search", site)
 
-website=st.radio(
-    "select your search platform",
-    ["DuckDuckGo", "Brave", "Google", "Youtube","Wikipedia"],
-    index=0,
-)
 
-if query:
-  match website:
-    case "Google": webbrowser.open_new_tab(
-        "https://www.google.com/search?q="+query
-        )
-    case "Brave": webbrowser.open_new_tab(
-        "https://search.brave.com/search?q="+query
-        )
-    case "DuckDuckGo": webbrowser.open_new_tab(
-        "https://duckduckgo.com/?q="+query
-        )
-    case "Youtube": webbrowser.open_new_tab(
-        "https://www.youtube.com/results?search_query="+query
-        )
-    case "Wikipedia": webbrowser.open_new_tab(
-        "https://en.wikipedia.org/wiki/"+query
-        )
-st.link_button("Go to gallery", "https://www.google.com")
+        
